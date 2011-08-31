a484b1e094b12d1486c7d6bae8ce6cd09679928c
Ray Allen <ray@ray-laptop.(none)>
2011-08-31 15:30:11 +0800

    ignore the data when sina weibo user id < 0 and gender is f or m
    Signed-off-by: Ray Allen <ray@ray-laptop.(none)>

--- letmewb/weibo/_gethots.php
+++ letmewb/weibo/_gethots.php
@@ -20,58 +20,60 @@ $hots =
 	);
 $i = 0;
 foreach ($hots as $hot) {
-	$sql = sprintf(
-		'insert into weibo_users (`uid`, `screen_name`, `name`, `province`, `city`, `location`, '
-		. '`description`, `url`, `profile_image_url`, `domain`, `gender`, `followers_count`, '
-		. '`friends_count`, `statuses_count`, `favourites_count`, `created_at`, `following`, '
-		. '`allow_all_act_msg`, `geo_enabled`, `verified`, `status_id`)'
-		. ' values (%d, "%s", "%s", %d, %d, "%s", '
-		. '"%s", "%s", "%s", "%s", "%s", %d, '
-		. '%d, %d, %d, "%s", null, '
-		. '%d, %d, %d, null)'
-		. ' on duplicate key'
-		. ' update `screen_name` = "%s", `name` = "%s", `province` = %d, `city` = %d, '
-		. ' `location` = "%s", `description` = "%s", `url` = "%s", `profile_image_url` = "%s", '
-		. ' `domain` = "%s", `gender` = "%s", `followers_count` = %d, `friends_count` = %d, '
-		. ' `statuses_count` = %d, `favourites_count` = %d, `created_at` = "%s", '
-		. ' `allow_all_act_msg` = %d, `geo_enabled` = %d, `verified` = %d;',
-		$hot['id'],
-		/*
-		 * the same 2 pieces below
-		 */
-		/*
-		 * piece NO.1
-		 */
-		mysql_real_escape_string($hot['screen_name']),
-		mysql_real_escape_string($hot['name']), $hot['province'], $hot['city'],
-		mysql_real_escape_string($hot['location']),
-		mysql_real_escape_string($hot['description']),
-		mysql_real_escape_string($hot['url']),
-		mysql_real_escape_string($hot['profile_image_url']),
-		mysql_real_escape_string($hot['domain']),
-		mysql_real_escape_string($hot['gender']), $hot['followers_count'], $hot['friends_count'],
-		$hot['statuses_count'],	$hot['favourites_count'],
-		mysql_real_escape_string($hot['created_at']),
-		$hot['allow_all_act_msg'], $hot['geo_enabled'], $hot['verified'],
-		/*
-		 * piece NO.2
-		 */
-		mysql_real_escape_string($hot['screen_name']),
-		mysql_real_escape_string($hot['name']), $hot['province'], $hot['city'],
-		mysql_real_escape_string($hot['location']),
-		mysql_real_escape_string($hot['description']),
-		mysql_real_escape_string($hot['url']),
-		mysql_real_escape_string($hot['profile_image_url']),
-		mysql_real_escape_string($hot['domain']),
-		mysql_real_escape_string($hot['gender']), $hot['followers_count'], $hot['friends_count'],
-		$hot['statuses_count'],	$hot['favourites_count'],
-		mysql_real_escape_string($hot['created_at']),
-		$hot['allow_all_act_msg'], $hot['geo_enabled'], $hot['verified']
-	);
-	mysql_query("set names 'utf8';");
-	mysql_query($sql, $zconn->dblink)
-		or die ("~failed~~db_err:" . mysql_error() . "~~failed~");
-	$i += mysql_affected_rows();
+	if (in_array('' . $hot['gender'], array('f', 'm')) && intval($hot['id']) >= 0) {
+		$sql = sprintf(
+			'insert into weibo_users (`uid`, `screen_name`, `name`, `province`, `city`, `location`, '
+			. '`description`, `url`, `profile_image_url`, `domain`, `gender`, `followers_count`, '
+			. '`friends_count`, `statuses_count`, `favourites_count`, `created_at`, `following`, '
+			. '`allow_all_act_msg`, `geo_enabled`, `verified`, `status_id`)'
+			. ' values (%d, "%s", "%s", %d, %d, "%s", '
+			. '"%s", "%s", "%s", "%s", "%s", %d, '
+			. '%d, %d, %d, "%s", null, '
+			. '%d, %d, %d, null)'
+			. ' on duplicate key'
+			. ' update `screen_name` = "%s", `name` = "%s", `province` = %d, `city` = %d, '
+			. ' `location` = "%s", `description` = "%s", `url` = "%s", `profile_image_url` = "%s", '
+			. ' `domain` = "%s", `gender` = "%s", `followers_count` = %d, `friends_count` = %d, '
+			. ' `statuses_count` = %d, `favourites_count` = %d, `created_at` = "%s", '
+			. ' `allow_all_act_msg` = %d, `geo_enabled` = %d, `verified` = %d;',
+			$hot['id'],
+			/*
+			 * the same 2 pieces below
+			 */
+			/*
+			 * piece NO.1
+			 */
+			mysql_real_escape_string($hot['screen_name']),
+			mysql_real_escape_string($hot['name']), $hot['province'], $hot['city'],
+			mysql_real_escape_string($hot['location']),
+			mysql_real_escape_string($hot['description']),
+			mysql_real_escape_string($hot['url']),
+			mysql_real_escape_string($hot['profile_image_url']),
+			mysql_real_escape_string($hot['domain']),
+			mysql_real_escape_string($hot['gender']), $hot['followers_count'], $hot['friends_count'],
+			$hot['statuses_count'],	$hot['favourites_count'],
+			mysql_real_escape_string($hot['created_at']),
+			$hot['allow_all_act_msg'], $hot['geo_enabled'], $hot['verified'],
+			/*
+			 * piece NO.2
+			 */
+			mysql_real_escape_string($hot['screen_name']),
+			mysql_real_escape_string($hot['name']), $hot['province'], $hot['city'],
+			mysql_real_escape_string($hot['location']),
+			mysql_real_escape_string($hot['description']),
+			mysql_real_escape_string($hot['url']),
+			mysql_real_escape_string($hot['profile_image_url']),
+			mysql_real_escape_string($hot['domain']),
+			mysql_real_escape_string($hot['gender']), $hot['followers_count'], $hot['friends_count'],
+			$hot['statuses_count'],	$hot['favourites_count'],
+			mysql_real_escape_string($hot['created_at']),
+			$hot['allow_all_act_msg'], $hot['geo_enabled'], $hot['verified']
+		);
+		mysql_query("set names 'utf8';");
+		mysql_query($sql, $zconn->dblink)
+			or die ("~failed~~db_err:" . mysql_error() . "~~failed~");
+		$i += mysql_affected_rows();
+	}
 }
 echo "Total: " . count($hots) . " / Affected: " . $i . "\n";
 ?>
