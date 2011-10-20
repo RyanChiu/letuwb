3469b9572b0909d04a19be8cbf30fdf3e8977465
Ray Allen <ray@ray-laptop.(none)>
2011-10-20 17:27:36 +0800

    test if the auto-update works.
    Signed-off-by: Ray Allen <ray@ray-laptop.(none)>

--- letmewb/ver.php
+++ letmewb/ver.php
@@ -2,6 +2,6 @@
 /*
  * 
  */
-$code = "1"; $name = "1.00.00";	// both are as the same as the ones in AndroidManifest.xml
+$code = "2"; $name = "1.00.01";	// both are as the same as the ones in AndroidManifest.xml
 echo "versionCode,$code,versionName,$name"
 ?>
