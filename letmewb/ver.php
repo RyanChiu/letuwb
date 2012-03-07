207fc78c4343eb3a96872bccef2dab1c5290da4c
Ray Allen <ray@ray-laptop.(none)>
2012-03-07 22:01:04 +0800

    a version that in the middle of updating
    change the server ip and don't use the domain name for the moment.
    
    Signed-off-by: Ray Allen <ray@ray-laptop.(none)>

--- letmewb/ver.php
+++ letmewb/ver.php
@@ -2,9 +2,12 @@
 /*
  * 
  */
-$code = "3"; $name = "1.00.02";	// both are as the same as the ones in AndroidManifest.xml
+$code = "4"; $name = "1.01.01";	// both are as the same as the ones in AndroidManifest.xml
 echo "versionCode,$code,versionName,$name,"
 	. "newContent,"
 	. "1.主界面所有菜单项增加“退出”。#"
-	. "2.直接点击大图可进入微博浏览界面。#";
+	. "2.直接点击大图可进入微博浏览界面。#"
+	. "3.微博页面可查看原始图片。#"
+	. "4.微博页面可查看动态GIF图片。#"
+	. "5.可保存微博页面内图片到本地SD卡。#";
 ?>
