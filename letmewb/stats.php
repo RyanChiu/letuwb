59612ea2e8933c0bbe3d9e5ae7de7ecc65c094d2
Ray Allen <ray@ray-laptop.(none)>
2012-04-09 15:32:48 +0800

    let stats.php could get the number of total users/pages by clientkey.
    let stats.php could get the number of total users/pages by clientkey.
    
    Signed-off-by: Ray Allen <ray@ray-laptop.(none)>

--- letmewb/stats.php
+++ letmewb/stats.php
@@ -3,7 +3,15 @@ include 'zmysqlConn.class.php';
 $zconn = new zmysqlConn;
 if (array_key_exists("total", $_GET) && $_GET['total'] == "pages"
 	&& array_key_exists("limit", $_GET) && intval($_GET['limit']) > 0) {
-	$sql = "select count(id) from weibo_users";
+	if (array_key_exists("clientkey", $_GET)) {
+		$sql = sprintf(
+			"select count(id) from weibo_users where uid in"
+			. " (select a.uid from possessions a, clients b where a.clientid = b.id and b.`key` = '%s')",
+			$_GET['clientkey']
+		);
+	} else {
+		$sql = "select count(id) from weibo_users";
+	}
 	$rs = mysql_query($sql, $zconn->dblink)
 		or die ("db_err: " . mysql_error());
 	$total = 0;
@@ -24,7 +32,15 @@ if (array_key_exists("total", $_GET) && $_GET['total'] == "pages"
 }
 
 if (array_key_exists("total", $_GET) && $_GET['total'] == "usrs") {
-	$sql = sprintf("select count(id) from weibo_users");
+	if (array_key_exists("clientkey", $_GET)) {
+		$sql = sprintf(
+				"select count(id) from weibo_users where uid in"
+		. " (select a.uid from possessions a, clients b where a.clientid = b.id and b.`key` = '%s')",
+		$_GET['clientkey']
+		);
+	} else {
+		$sql = sprintf("select count(id) from weibo_users");
+	}
 	$rs = mysql_query($sql, $zconn->dblink)
 		or die ("db_err: " . mysql_error());
 	$total = 0;
