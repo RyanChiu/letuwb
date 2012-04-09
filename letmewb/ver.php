b209c7b0e17d4f36f9f4d18b3c0f2ed7e41ec11b
Ray Allen <ray@ray-laptop.(none)>
2012-04-09 18:12:00 +0800

    version update: version code 6, version name 1.02.00
    version update: version code 6, version name 1.02.00
    
    Signed-off-by: Ray Allen <ray@ray-laptop.(none)>

--- letmewb/ver.php
+++ letmewb/ver.php
@@ -2,12 +2,14 @@
 /*
  * 
  */
-$code = "5"; $name = "1.01.02";	// both are as the same as the ones in AndroidManifest.xml
+$code = "6"; $name = "1.02.00";	// both are as the same as the ones in AndroidManifest.xml
 echo "versionCode,$code,versionName,$name,"
 	. "newContent,"
 	. "1.主界面所有菜单项增加“退出”。#"
 	. "2.直接点击大图可进入微博浏览界面。#"
 	. "3.微博页面可查看原始图片。#"
 	. "4.微博页面可查看动态GIF图片。#"
-	. "5.可保存微博页面内图片到本地SD卡。#";
+	. "5.可保存微博页面内图片到本地SD卡。#"
+	. "6.微博页面也可以投票了。#"
+	. "7.只要登陆微博帐号，在任何别的终端上的私藏都将统统合并在一起。#";
 ?>
