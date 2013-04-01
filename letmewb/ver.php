c16cfaf63e0772386774ccb3bb31ad3cf296baf5
Ray Allen <ray@ray-Lenovo-IdeaPad-S400.(none)>
2013-04-01 09:43:16 +0800

    version update: version code 9, version name 3.0.
    remove all the old OAuth1.0 stuff and replace it with OAuth2.0, and implement
    all the old functions all over again.
    
    Signed-off-by: Ray Allen <ray@ray-Lenovo-IdeaPad-S400.(none)>

--- letmewb/ver.php
+++ letmewb/ver.php
@@ -2,14 +2,10 @@
 /*
  * 
  */
-$code = "8"; $name = "2.01.01";	// both are as the same as the ones in AndroidManifest.xml
+$code = "9"; $name = "3.0";	// both are as the same as the ones in AndroidManifest.xml
 echo "versionCode,$code,versionName,$name,"
 	. "newContent,"
-	. "1.主界面采用PINTEREST形式，内容更加丰富。#"
-	. "2.主界面所有菜单项增加“退出”选项。#"
-	. "3.直接点击大图可进入微博浏览界面。#"
-	. "4.微博页面也可以投票了。#"
-	. "5.只要登陆微博帐号，在任何别的终端上的私藏都将统统合并在一起。#"
-	. "6.微博页面可查看原始图片（包括动态GIF图片）。#"
-	. "7.可保存微博页面内图片到本地SD卡。#";
+	. "1.采用新浪微博全新OAuth2认证方式，应用更安全。#"
+	. "2.浏览用户微博时点击其头像（左上角）查看头像的大图。#"
+	. "3.解决由于微博被删除后由于用户信息不存在而导致的异常退出问题。#";
 ?>
